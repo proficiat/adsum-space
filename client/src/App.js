63b2ae38d659ea65a47f21727bfdb10dd4306239
proficiat <sereja.korotenko@gmail.com>
2018-01-03 19:47:48 +0200

    added react-router

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 import IdeasContainer from './components/IdeasContainer'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+)
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Idea Board</h1>
-        </header>
-        <IdeasContainer />
-      </div>
-    );
+      <Router>
+        <div className="App">
+          <ul className="navigationBar">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/ideas">Ideas</Link>
+            </li>
+          </ul>
+          <div className="pageContent">
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/ideas" component={IdeasContainer} />
+          </div>
+        </div>
+      </Router>
+    )
   }
 }
 
-export default App;
+export default App
